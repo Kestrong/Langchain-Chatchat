1d700499f5793499772d3a2223f8762598cf1eea
keshc <keshc@ffcs.cn>
2025-02-25 20:12:24 +0800

    修复embedding超长问题

--- server/model_workers/qwen.py
+++ server/model_workers/qwen.py
@@ -1,6 +1,7 @@
 import sys
 from typing import List, Literal, Dict
 
+import numpy as np
 from fastchat import conversation as conv
 from fastchat.conversation import Conversation, SeparatorStyle
 from openai import OpenAI
@@ -62,10 +63,15 @@ class QwenWorker(ApiModelWorker):
                 self.logger.error(f"请求千问 API 时发生错误：{data}")
                 yield data
 
+    def split_string_by_length(self, s, length):
+        return [s[i:i + length] for i in range(0, len(s), length)]
+
     def do_embeddings(self, params: ApiEmbeddingsParams) -> Dict:
         params.load_config(self.model_names[0])
         if log_verbose:
             logger.info(f'{self.__class__.__name__}:params: {params}')
+        overlap_method = params.role_meta.get("overlap_method")
+        max_length = params.role_meta.get("max_embedding_length", 512)
         with OpenAI(
                 api_key=params.api_key,  # 如果您没有配置环境变量，请在此处用您的API Key进行替换
                 base_url=params.api_proxy,  # 填写DashScope服务的base_url
@@ -74,13 +80,51 @@ class QwenWorker(ApiModelWorker):
             try:
                 result = []
                 i = 0
+                chunk_lens = {}
+                chunk_index = {}
                 while i < len(params.texts):
                     texts = params.texts[i:i + 25]
+                    if overlap_method == "truncate":
+                        texts = [x[:max_length] if len(x) > max_length else x for x in texts]
+                    elif overlap_method == "chunk":
+                        new_texts = []
+                        for index, t in enumerate(texts, start=0):
+                            if len(t) > max_length:
+                                part_len = []
+                                chunk_num = 0
+                                for chunk in self.split_string_by_length(t, length=max_length):
+                                    chunk_num += 1
+                                    new_texts.append(chunk)
+                                    part_len.append(len(chunk))
+                                chunk_lens[len(new_texts) - chunk_num] = part_len
+                                chunk_index[len(new_texts) - chunk_num] = chunk_num
+                            else:
+                                new_texts.append(t)
+                        texts = new_texts
+
                     resp = client.embeddings.create(
                         model=params.embed_model or self.DEFAULT_EMBED_MODEL,
                         input=texts,  # 最大25行
                     )
                     embeddings = [x.embedding for x in resp.data]
+                    if overlap_method == "chunk" and len(chunk_index) > 0:
+                        new_embeddings = []
+                        start_index = 0
+                        while start_index < len(embeddings):
+                            if start_index not in chunk_index:
+                                new_embeddings.append(embeddings[start_index])
+                                start_index += 1
+                            else:
+                                chunk_embeddings = np.average(
+                                    np.array(embeddings[start_index:start_index + chunk_index[start_index]]), axis=0,
+                                    weights=chunk_lens[start_index])
+                                chunk_embeddings = chunk_embeddings / np.linalg.norm(chunk_embeddings)
+                                chunk_embeddings = chunk_embeddings.tolist()
+                                new_embeddings.append(chunk_embeddings)
+                                start_index += chunk_index[start_index]
+                        embeddings = new_embeddings
+                    chunk_index.clear()
+                    chunk_lens.clear()
                     result += embeddings
                     i += 25
             except Exception as e:
