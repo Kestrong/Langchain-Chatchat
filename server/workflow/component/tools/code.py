c42ad8911a20af1af4c2a58233ea1fedc55aceb7
keshc <keshc@ffcs.cn>
2025-04-09 14:58:13 +0800

    优化工作流组件参数的变量解析和代码执行节点

--- server/workflow/component/tools/code.py
+++ server/workflow/component/tools/code.py
@@ -1,10 +1,20 @@
+import multiprocessing
 from typing import Dict, Any, Union
 
-from langchain_experimental.utilities import PythonREPL
-
+from configs import PYTHON_REPL_TIMEOUT
 from server.workflow.component.base.component import Component
-from server.workflow.utils.inputs import TextInput
+from server.workflow.utils.inputs import TextInput, DictInput
 from server.workflow.utils.outputs import DictOutput
+
+
+def exec_python(python_code: str, args: Dict[str, Any], _globals: Dict[str, Any],
+                _locals: Dict[str, Any], queue: multiprocessing.Queue) -> Any:
+    try:
+        exec(python_code, _globals, _locals)
+        result = _locals['main'](**args)
+        queue.put(result)
+    except Exception as e:
+        queue.put(repr(e))
 
 
 class PythonREPLComponent(Component):
@@ -15,11 +25,20 @@ class PythonREPLComponent(Component):
     icon: Union[str, None]
 
     inputs = [
+        DictInput(
+            name='args',
+            display_name='Args',
+            info='The args for python function.',
+            value={"arg1": 1, "arg2": 2}
+        ),
         TextInput(
             name='python_code',
             display_name='Python',
             required=True,
-            info='python code.'
+            info='python code.',
+            value="""def main(arg1: int, arg2: int) -> dict:
+                return arg1 + arg2
+            """
         )
     ]
 
@@ -30,10 +49,22 @@ class PythonREPLComponent(Component):
         )
     ]
 
-    async def _run(self, state: Dict[str, Any]) -> Dict[str, Any]:
-        py_repl = PythonREPL()
+    def _run(self, state: Dict[str, Any]) -> Dict[str, Any]:
+        self.update_input_context()
         inputs = self.get_context()[self.id]["inputs"]
         python_code = inputs.get("python_code")
         del inputs["python_code"]
-        result = py_repl.run(python_code)
+        args = inputs.get("args")
+        _globals = {}
+        _locals = {}
+        queue: multiprocessing.Queue = multiprocessing.Queue()
+        p = multiprocessing.Process(
+            target=exec_python, args=(python_code, args, _globals, _locals, queue)
+        )
+        p.start()
+        if PYTHON_REPL_TIMEOUT > 0:
+            p.join(PYTHON_REPL_TIMEOUT)
+        if p.is_alive():
+            p.terminate()
+        result = queue.get()
         return {"result": result}
