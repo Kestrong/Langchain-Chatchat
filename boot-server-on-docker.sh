b875b92e4eaff85b9185247c106860c49de47505
keshc <ffkeshc@chinatelecom.cn>
2025-11-06 11:35:58 +0800

    新增菜单字段

--- boot-server-on-docker.sh
+++ boot-server-on-docker.sh
@@ -1,5 +1,4 @@
 # shellcheck disable=SC2164
 cd "${PROJECT_DIR}"
 rm -rf logs/model_worker_*.log
-cp -r ${PROJECT_DIR}/template/* ${PROJECT_DIR}/knowledge_base/samples/content/template/
 python startup.py "${ARGS}"
--- server/db/repository/chat_menu_repository.py
+++ server/db/repository/chat_menu_repository.py
@@ -8,9 +8,9 @@ from server.memory.token_info_memory import get_token_info
 
 @with_session
 def add_menu_to_db(session, menu_name: str, menu_name_en: str, menu_icon: str, url: str, auth_level: int, enabled: str,
-                   sort_id: int):
+                   sort_id: int, auth_users:str):
     c = ChatMenuModel(menu_name=menu_name, menu_name_en=menu_name_en, menu_icon=menu_icon, url=url,
-                      auth_level=auth_level, enabled=enabled,
+                      auth_level=auth_level, enabled=enabled, auth_users=auth_users,
                       create_by=get_token_info().get("userId"), sort_id=sort_id)
     session.add(c)
     session.flush()
@@ -19,8 +19,7 @@ def add_menu_to_db(session, menu_name: str, menu_name_en: str, menu_icon: str, u
 
 @with_session
 def update_menu_to_db(session, menu_id: int, menu_name: str, menu_name_en: str, menu_icon: str, url: str,
-                      auth_level: int, enabled: str,
-                      sort_id: int):
+                      auth_level: int, enabled: str, auth_users:str ,sort_id: int):
     menu: ChatMenuModel = session.query(ChatMenuModel).filter(ChatMenuModel.id == menu_id).first()
     if menu is not None:
         menu.menu_name = menu_name
@@ -30,6 +29,7 @@ def update_menu_to_db(session, menu_id: int, menu_name: str, menu_name_en: str, 
         menu.auth_level = auth_level
         menu.enabled = enabled
         menu.sort_id = sort_id
+        menu.auth_users = auth_users
     else:
         raise ValueError("ChatMenu with id {} does not exist".format(menu))
     return menu.id
