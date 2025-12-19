2f6c9151ca1744c6b52c739d24729c481f79e071
keshc <ffkeshc@chinatelecom.cn>
2025-12-19 09:27:04 +0800

    启明工作流增加返回内容为空处理

--- server/model_workers/qiming.py
+++ server/model_workers/qiming.py
@@ -301,14 +301,13 @@ class QimingWorker(ApiModelWorker):
                         answer_key = model_config.get('output_key') or params.role_meta.get("output_key")
                         data = response.json().get('data', {})
                         outputs = data.get('outputs') or {}
+                        answer = ''
                         if answer_key and answer_key in outputs:
                             answer = outputs.get(answer_key, '')
                         else:
-                            if len(outputs) == 0:
-                                answer = '启明平台没有任何回复内容'
-                            elif len(outputs) == 1:
+                            if len(outputs) == 1:
                                 answer = list(outputs.values())[0]
-                            else:
+                            elif len(outputs) > 1:
                                 answer = json.dumps(outputs, ensure_ascii=False)
                         if not answer and data.get('error'):
                             text = data.get('error')
